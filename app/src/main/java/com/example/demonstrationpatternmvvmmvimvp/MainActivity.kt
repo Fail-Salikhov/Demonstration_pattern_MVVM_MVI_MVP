1d1893243391bdde987a2b7b1a3007dd284b8f65
Fail Salikhov <sff1313@yandex.ru>
2023-03-07 23:13:51 +0400

    refactor

--- app/src/main/java/com/example/demonstrationpatternmvvmmvimvp/MainActivity.kt
+++ app/src/main/java/com/example/demonstrationpatternmvvmmvimvp/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.demonstrationpatternmvvmmvimvp
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.demonstrationpatternmvvmmvimvp.databinding.ActivityMainBinding
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,11 +16,22 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
-        binding.button.setOnClickListener {
+        binding.buttonNumber.setOnClickListener {
             data.counter()
             binding.textView.text = data.count.toString()
         }
 
+        binding.buttonColor.setOnClickListener {
+            setRandomColor()
+        }
+    }
 
+    private fun setRandomColor() {
+        val randomColor = Color.rgb(
+            Random.nextInt(256),
+            Random.nextInt(256),
+            Random.nextInt(256)
+        )
+        binding.textView.setTextColor(randomColor)
     }
 }
